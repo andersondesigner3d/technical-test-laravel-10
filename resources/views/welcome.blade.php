afe048a69fe5155558d5b64f4b93d1c93f97fe37
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2023-10-10 16:24:56 -0300

    primeiro-commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <h1>Exemplo de Bootstrap</h1>
+  <p>
+    Este é um exemplo de como usar o Bootstrap em uma view Laravel.
+  </p>
+  <button type="button" class="btn btn-primary">
+    Botão Bootstrap de Teste
+  </button>
+</div>
+
+@endsection
